21a4b2e8bbfcdad87d2fec15dadbd2c5254b3f0f
parth3083 <parthbrajput30@gmail.com>
2024-03-06 10:11:59 +0530

    "updated the navbar"

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import '../Navbar/navbar.scss'
 function Navbar() {
   return (
       <div className='main'>
-              <nav>
+              <nav className='w-full h-[10vh] flex items-center justify-between px-[4vw] py-[2vw] fixed top-0'>
     <Link to='/home'>
-    <div id="nav-left">
+    <div id="nav-left" className=' w-fit  h-[3.25vw] overflow-hidden'>
         <h1>Nexus</h1>
         <h1>Nexus</h1>
     </div></Link>
-    <div id="nav-right">
+    <div id="nav-right" className='w-fit  h-[2vw] overflow-hidden'>
         <h3>info@nexus.com</h3>
         <h3>info@nexus.com</h3>
     </div>
